3b3f45564bd83a25a09fbb7275cb1199d3b3a072
Stronn1 <115506952+Stronn1@users.noreply.github.com>
2022-10-10 18:45:23 -0300

    Update driver.py

--- simple_bc_all/driver.py
+++ simple_bc_all/driver.py
@@ -5,47 +5,19 @@ from pyke import knowledge_engine
 from pyke import krb_traceback
 
 engine = knowledge_engine.engine(__file__)
-
-
-def bc_test():
-
-    engine.reset()      # Allows us to run tests multiple times.
-
-    engine.activate('bc_simple_rules') #STUDENTS: you will need to edit the name of your rule file here
-
-    print("doing proof")
-    
-    try:
-        with engine.prove_goal('bc_simple_rules.what_to_bring($bring)') as gen: #STUDENTS: you will need to edit this line
-            for vars, plan in gen:
-                print("You should bring: %s" % (vars['bring'])) #STUDENTS: you will need to edit this line
-    except Exception:
-        # This converts stack frames of generated python functions back to the
-        # .krb file.
-        krb_traceback.print_exc()
-        sys.exit(1)
-
-    print()
-    print("done")
-    #engine.print_stats()
-
     
 def bc_test_questions():
 
-    engine.reset()      # Allows us to run tests multiple times.
+    engine.reset()      
 
-    engine.activate('rules') #STUDENTS: you will need to edit the name of your rule file here
-
-    print("doing proof")
+    engine.activate('rules') 
     
     try:
-        with engine.prove_goal('rules.especie($esp)') as gen: #STUDENTS: you will need to edit this line
+        with engine.prove_goal('rules.especie($esp)') as gen: 
             for vars, plan in gen:
-                print("Especie: %s" % (vars['esp'])) #STUDENTS: you will need to edit this line
+                print("Especie: %s" % (vars['esp'])) 
 
     except Exception:
-        # This converts stack frames of generated python functions back to the
-        # .krb file.
         krb_traceback.print_exc()
         sys.exit(1)
 
